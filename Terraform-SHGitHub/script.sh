9031ba665d3668886ba3d4ee3fb477304d5530d7
Lior Lavi <liorlavi1@gmail.com>
2024-02-13 20:42:45 +0200

    Removed token in script.sh

--- Terraform-SHGitHub/script.sh
+++ Terraform-SHGitHub/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # curl -o actions-runner-linux-x64-2.313.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.313.0/actions-runner-linux-x64-2.313.0.tar.gz
 # tar xzf actions-runner-linux-x64-2.313.0.tar.gz
-# ./config.sh --url https://github.com/tagaldev/react-java0mysql --token BFVMDXIYH2URYF4LWVRRTBTFZO4OY
+# ./config.sh --url https://github.com/tagaldev/react-java0mysql
 # ./run.sh
