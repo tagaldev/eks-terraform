1d57e299605287c6a9cd66bed886efaa935ad489
Lior Lavi <liorlavi1@gmail.com>
2024-02-14 00:23:18 +0200

    Added tf files to add CSI driver to EKS ( PV is now working ) && fixed jfrog user data script ( also working now. need to wait 5 minutes after terraform apply )

--- Terraform-JFrog/script.sh
+++ Terraform-JFrog/script.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-apt update -y
-apt install -y default-jdk
+sudo apt update -y
+sudo apt install -y default-jdk
 java -version
 curl -LsS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash -s --
-apt update
-apt install mariadb-server mariadb-client -y
-systemctl start mariadb
-systemctl enable mariadb
+sudo apt update
+sudo apt install mariadb-server mariadb-client -y
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+sudo chmod 777 /etc/apt/sources.list.d/artifactory.list
 echo "deb https://releases.jfrog.io/artifactory/artifactory-debs xenial main" | tee -a /etc/apt/sources.list.d/artifactory.list
-curl -fsSL  https://releases.jfrog.io/artifactory/api/gpg/key/public|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/artifactory.gpg
-apt update -y
-apt install jfrog-artifactory-oss
-systemctl start artifactory.service 
-systemctl enable artifactory.service
+sudo wget -qO - https://releases.jfrog.io/artifactory/api/gpg/key/public | sudo apt-key add -
+echo "deb https://releases.jfrog.io/artifactory/artifactory-debs focal main" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update -y
+sudo apt-get install jfrog-artifactory-oss -y
+sudo systemctl start artifactory.service
 systemctl status artifactory.service
-apt install -y net-tools
+sudo apt install -y net-tools
