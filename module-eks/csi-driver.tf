97a05cdfa5b0af07811d5f919fc8879177667e96
Lior Lavi <liorlavi1@gmail.com>
2024-02-15 23:37:04 +0200

    Fixed Grafana && CSI driver for EKS

--- module-eks/csi-driver.tf
+++ module-eks/csi-driver.tf
@@ -31,4 +31,5 @@ resource "aws_eks_addon" "csi_driver" {
   addon_name               = "aws-ebs-csi-driver"
   addon_version            = "v1.26.1-eksbuild.1"
   service_account_role_arn = aws_iam_role.eks_ebs_csi_driver.arn
+  resolve_conflicts = "OVERWRITE"
 }
